633edbda392f4134aa35b47ca50f751ec7a70691
davidmathias <thestardavid@gmail.com>
2025-08-04 16:42:02 +0530

    bkp

--- env.sh
+++ env.sh
@@ -1,5 +1,7 @@
 export JAVA_TOOL_OPTIONS="-javaagent:/home/davidmathias/work/otel-example/opentelemetry-javaagent.jar" \
-  OTEL_TRACES_EXPORTER=logging \
-  OTEL_METRICS_EXPORTER=logging \
+  OTEL_TRACES_EXPORTER=otlp \
+  OTEL_METRICS_EXPORTER=otlp \
   OTEL_LOGS_EXPORTER=logging \
-  OTEL_METRIC_EXPORT_INTERVAL=15000
+  OTEL_METRIC_EXPORT_INTERVAL=3000 \
+  OTEL_SERVICE_NAME="my-custom-service2-java" \
+  OTEL_EXPORTER_OTLP_ENDPOINT=http://localhost:4318 \
