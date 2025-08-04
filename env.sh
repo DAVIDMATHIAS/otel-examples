295af6f5c9101024e4d3ed8068f617f78379cd5f
davidmathias <thestardavid@gmail.com>
2025-08-04 16:58:26 +0530

    OTEL Custom attrs

--- env.sh
+++ env.sh
@@ -1,7 +1,2 @@
-export JAVA_TOOL_OPTIONS="-javaagent:/home/davidmathias/work/otel-example/opentelemetry-javaagent.jar" \
-  OTEL_TRACES_EXPORTER=otlp \
-  OTEL_METRICS_EXPORTER=otlp \
-  OTEL_LOGS_EXPORTER=logging \
-  OTEL_METRIC_EXPORT_INTERVAL=3000 \
-  OTEL_SERVICE_NAME="my-custom-service2-java" \
-  OTEL_EXPORTER_OTLP_ENDPOINT=http://localhost:4318 \
+export OTEL_SERVICE_NAME="graavl_dvd_example_service"
+export OTEL_EXPORTER_OTLP_ENDPOINT="http://localhost:4318"
---
+++ src/main/java/otel/Controller.java
@@ -0,0 +1,13 @@
+package otel;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+}
