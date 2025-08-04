f28b3c5ce5fb135ab606d056f2a734628ebca083
davidmathias <thestardavid@gmail.com>
2025-08-04 14:17:06 +0530

    backup

---
+++ env.sh
@@ -0,0 +1,5 @@
+export JAVA_TOOL_OPTIONS="-javaagent:/home/davidmathias/work/otel-example/opentelemetry-javaagent.jar" \
+  OTEL_TRACES_EXPORTER=logging \
+  OTEL_METRICS_EXPORTER=logging \
+  OTEL_LOGS_EXPORTER=logging \
+  OTEL_METRIC_EXPORT_INTERVAL=15000
